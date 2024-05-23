e7e0df1a6d90ed7d084f6bd3d37bd0631820d0a6
heizence <heizence6626@gmail.com>
2024-05-23 17:27:58 +0900

    add socket server,, update codes

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require("express");
-const http = require("http"); // for test on local
+const http = require("http");
 
 const bodyParser = require("body-parser");
 const { mainServerRouter } = require("./mainServerRouters.js");
-
+const { runSignalingServer } = require("./socketServer.js");
 require("dotenv").config();
 
 const { port } = process.env;
 
 const app = express();
 
+// options when using https
+// const options = {
+//   key: "./sslKey.pem",
+//   cert: "./sslCert.pem",
+// };
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Register the route
 app.use("/", mainServerRouter);
 
-// Create server
-http.createServer(options, app).listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, () => {
+  console.log(`[app.js]Server running at http://localhost:${port}`);
 });
+
+runSignalingServer(httpServer);
